0281c251be2d06528aea937def057bc1dcda605d
brittany melton <brittanymelton26@gmail.com>
2022-02-12 16:27:14 -0500

    completed variables exercises 1-3

--- app.js
+++ app.js
@@ -4,7 +4,22 @@ console.log(
 );
 
 // Exercise 1
-
+const firstName = "Brittany";
+let lastName = "Melton";
+var age = 28;
+let city = "Columbus";
+let pastTime = "watching Netflix"
 // Exercise 2
+let fullName = firstName + " " + lastName;
+console.log(fullName)
 
+//Bonus
+
+fullName = `${firstName} ${lastName}`;
 // Exercise 3
+
+let myStory = `Hello my name is ${fullName}. 
+I live in ${city}. 
+I enjoy ${pastTime}.`;
+
+console.log(myStory);
